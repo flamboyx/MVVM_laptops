fd2254239fd4b7405ff9c7b32452f152ca68f111
Timofey Trepachenko <tretim2014@gmail.com>
2024-04-18 02:48:42 +1000

    added fragments

--- app/src/main/java/com/example/mvvmlaptops/Laptop.kt
+++ app/src/main/java/com/example/mvvmlaptops/Laptop.kt
@@ -2,6 +2,8 @@ package com.example.mvvmlaptops
 
 data class Laptop(
     val model: String,
+    val manufacturer: String,
     val cpu: String,
+    val cpuManufacturer: String,
     val ram: String
 )
